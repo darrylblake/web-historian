912ae2eba3e157995f85bf5cd264edb436fda48a
Hack Reactor Students <accounts@hackreactor.com>
2015-07-22 16:24:36 -0700

    Returning index.html

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,14 +1,31 @@
 var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
+var helpers = require('./http-helpers.js');
 
 // Why do you think we have this here?
 // HINT: It has to do with what's in .gitignore
 initialize();
 
-var port = 8080;
+var port = 3000;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+// var server = http.createServer(handler.handleRequest);
+
+var routes = {
+  '/': function(req, res){
+    helpers.serveAssets(res, 'index.html', function(data, statusCode) {
+      helpers.sendResponse(res, data, statusCode);
+    });
+  }
+};
+
+var server = http.createServer(function (req, res) {
+  // console.log("Serving request type " + req.method + " for url " + req.url);
+  var path = helpers.getPathName(req);
+  var route = routes[path];
+  if (route) route(req, res);
+  else helpers.sendResponse(res, 'Not found!', 404);
+});
 
 if (module.parent) {
   module.exports = server;
@@ -16,4 +33,5 @@ if (module.parent) {
   server.listen(port, ip);
   console.log("Listening on http://" + ip + ":" + port);
 }
+
 
--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var parseUrl = require('url');
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -10,12 +11,35 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+// Todo: Get the root directory
+// var topModule = module;
+// while(topModule.parent)
+//   topModule = topModule.parent;
+// var appDir = path.dirname(topModule.filename);
+
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+
+  var filePath = '/Users/student/Codes/2015-06-web-historian/web/public/' + asset;
+
+  console.log(filePath);
+
+  fs.readFile(filePath, function (err, data) {
+    if (err) callback('Not found!', 404);
+    else callback(data, 200);
+  });
 };
 
+exports.getPathName = function (req) {
+  return parseUrl.parse(req.url).pathname;
+};
 
+exports.sendResponse = function (res, data, statusCode) {
+  console.log(data);
+  res.writeHead(statusCode, exports.headers);
+  res.end(data);
+};
 
 // As you progress, keep thinking about what helper functions you can put here!
